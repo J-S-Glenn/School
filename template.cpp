b7d779ddba5ee9569d63fbb3f9c55a1c85ce2244
John S Glenn Jr <152113379+J-S-Glenn@users.noreply.github.com>
2024-01-14 20:38:06 -0800

    Adding assignment template

---
+++ template.cpp
@@ -0,0 +1,36 @@
+// C++Tem.cpp -- Example C++ code template
+// 1/1/2020	*Assignment Date*
+// John Glenn
+
+#include <iostream>		// Any other libraties, too
+using namespace std;
+
+struc somestruct {
+
+};
+
+Class someclass {
+
+};
+
+// Global Variables
+
+// Function Prototypes
+void ProgramGreeting();		// All programs will have this method
+void Unittest();			// All P400 programs should have this, too
+
+
+int main() {
+	ProgramGreeting();		// Should go somewhere around here
+	Unittest();				// Follows ProgrmGreeting
+	
+}
+
+
+void ProgramGreeting() {
+// Code goes here
+}
+
+void Unittest() {
+// All test code here
+}
