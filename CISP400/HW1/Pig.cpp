5c76e535d7ad400d18a530eb5f39a87faa9d6ff9
John S Glenn Jr <152113379+J-S-Glenn@users.noreply.github.com>
2024-08-31 15:53:26 -0700

    Adding more requirements to pig

--- CISP400/HW1/Pig.cpp
+++ CISP400/HW1/Pig.cpp
@@ -17,36 +17,28 @@ class jlogger{
 		string F_START = "Started";
 		string F_END = "Ended";
 		ofstream obLogFile;
-
-		void initLogFile(){
-			time_t t = std::time(nullptr);
-			tm tm = *std::localtime(&t);
-			cout << "New log started at " << put_time(&tm, "%F %T") << endl;
-			obLogFile << "New log started at " << put_time(&tm, "%F %T") << endl;
-		}
-
+		void initLogFile();
 	public:
         static jlogger& getInstance(){
-			      static jlogger instance;
-				  return instance;
-        }
-        
-		void initialize(string app_Name = "My Program", string auth_Name = "John S. Glenn Jr", string logfile = "log.txt"){
+			static jlogger instance;
+			return instance;
+		}
+		void initialize(string app_Name = "My Program", string auth_Name = "John S. Glenn", string logfile = "log.txt"){
 			appName = app_Name;
 			authName = auth_Name;
 			log_file = logfile;
 			obLogFile.open(log_file);
 			initLogFile();
 			ProgramGreeting();
-        }
-
-		void announceFunction(string funcName, string action){
-			time_t t = std::time(nullptr);
-			tm tm = *std::localtime(&t);
-			obLogFile <<  put_time(&tm, "%F %T") <<  "\tFunction " << funcName << "() " << action << "\n";
 		}
 
-		void ProgramGreeting() {
+//Specification B2 - Function Activity to Disk
+		void announceFunction(string funcName, string action){
+	time_t t = std::time(nullptr);
+	tm tm = *std::localtime(&t);
+	obLogFile <<  put_time(&tm, "%F %T") <<  "\tFunction " << funcName << "() " << action << "\n";
+}
+		void ProgramGreeting(){
 			announceFunction(__func__, F_START);
 			std::time_t t = std::time(nullptr);
 			std::tm tm = *std::localtime(&t);
@@ -68,13 +60,16 @@ class SpontaneousDigitDispenser{
 	const static int DEFAULT_MAX = 10;
     int intValue = 0;
     float fltValue = 0;
+	// Specification C1 - Fixed Seed
+	unsigned int seed = 12345;
+	//unsigned int seed = time(NULL);
 
     SpontaneousDigitDispenser(){
-      std::srand(time(nullptr));  // Seed with current time
+      std::srand(seed);  // Seed with current time
     }
 
   public:
-    static SpontaneousDigitDispenser& getInstance() {
+    static SpontaneousDigitDispenser& getInstance(){
       static SpontaneousDigitDispenser instance;
           return instance;
       }
@@ -91,31 +86,93 @@ class SpontaneousDigitDispenser{
       fltValue = (min + static_cast<double>(randInt) / RAND_MAX * (max - min));
       return fltValue;
     }
+
+	int coinFlip(){
+		return dispenseInt(0,1);
+	}
 };
 
 class pigGame{
 	private:
+		enum action{
+			ROLL,
+			HOLD,
+			QUIT
+		};
+		enum player{
+			PLAYER,
+			AI
+		};
+		const string PIG_ART = R"(         ,.
+        (_|,.
+       ,' /, )_______   _
+    __j o``-'        `.'-)'
+   (")                 \'
+    `-j                |
+      `-._(           /
+         |_\  |--^.  /
+        /_]'|_| /_)_/
+           /_]'  /_]')""\n";
+		string retry_menu = R"( .----------------. .----------------. .----------------. .----------------.                                       
+| .--------------. | .--------------. | .--------------. | .--------------. |                                      
+| |   ______     | | |   _____      | | |      __      | | |  ____  ____  | |                                      
+| |  |_   __ \   | | |  |_   _|     | | |     /  \     | | | |_  _||_  _| | |                                      
+| |    | |__) |  | | |    | |       | | |    / /\ \    | | |   \ \  / /   | |                                      
+| |    |  ___/   | | |    | |   _   | | |   / ____ \   | | |    \ \/ /    | |                                      
+| |   _| |_      | | |   _| |__/ |  | | | _/ /    \ \_ | | |    _|  |_    | |                                      
+| |  |_____|     | | |  |________|  | | ||____|  |____|| | |   |______|   | |                                      
+| |              | | |              | | |              | | |              | |                                      
+| '--------------' | '--------------' | '--------------' | '--------------' |                                      
+ '----------------' '----------------' '----------------' '----------------'                                       
+ .----------------. .----------------. .----------------. .----------------. .-----------------..----------------. 
+| .--------------. | .--------------. | .--------------. | .--------------. | .--------------. | .--------------. |
+| |      __      | | |    ______    | | |      __      | | |     _____    | | | ____  _____  | | |    ______    | |
+| |     /  \     | | |  .' ___  |   | | |     /  \     | | |    |_   _|   | | ||_   \|_   _| | | |   / _ __ `.  | |
+| |    / /\ \    | | | / .'   \_|   | | |    / /\ \    | | |      | |     | | |  |   \ | |   | | |  |_/____) |  | |
+| |   / ____ \   | | | | |    ____  | | |   / ____ \   | | |      | |     | | |  | |\ \| |   | | |    /  ___.'  | |
+| | _/ /    \ \_ | | | \ `.___]  _| | | | _/ /    \ \_ | | |     _| |_    | | | _| |_\   |_  | | |    |_|       | |
+| ||____|  |____|| | |  `._____.'   | | ||____|  |____|| | |    |_____|   | | ||_____|\____| | | |    (_)       | |
+| |              | | |              | | |              | | |              | | |              | | |              | |
+| '--------------' | '--------------' | '--------------' | '--------------' | '--------------' | '--------------' |
+ '----------------' '----------------' '----------------' '----------------' '----------------' '----------------' )";
 		SpontaneousDigitDispenser& dice = dice.getInstance();
+		int WIN = 100000;
 		int game_count;
+		bool playAgain;
+		bool turnEnd;
+		int roll = 0;
+		int player_grand_total = 0;
+		int ai_grand_total = 0;
+		int player_total = 0;
+		int ai_total = 0;
+		action actionChoice;
+		player currentPlayer;
 		string F_START = "Started";
 		string F_END = "Ended";
+		string hPlayer;
+		string FG_RED = "\033[1;31m";
+		string RST = "\033[0m";
+		// Specification C4 - Three Functions
 		jlogger& myScribe = myScribe.getInstance();
-			void exitGame(){
-			myScribe.announceFunction(__func__,"Game Exiting...");
-		}
+		int promptIntInRange(string, int, int);
+		bool promptYN(string);
+        bool isInt(string);
+		string getPrompt(string prompt);
+		bool exitGame();
 		int D6();
 		int RandomNumber(int, int);
+		void getPlayerName();
+		void performAction();
+		void getAction();
+		void playerTurn();
+		void startGame();
+		void getCurrentScores();
+		void rollDice();
+		void AITurn();
+		void welcome();
+		void getFirstPlayer();
 	public:
-		pigGame(){
-			myScribe.announceFunction(__func__, "Initializing Pig");
-			int someNumber = D6();
-			myScribe.announceFunction(__func__, "Playing Pig");
-			someNumber = D6();
-			someNumber = D6();
-			someNumber = D6();
-			myScribe.announceFunction(__func__, "Exiting Pig");
-		}
-
+		pigGame();
 
 };
 
@@ -137,16 +194,84 @@ int main() {
   	myScribe.announceFunction(__func__, "Total runtime: " + to_string(total_runtime) + " sec");
 }
 
+int pigGame::promptIntInRange(string prompt, int min, int max)
+{
+  myScribe.announceFunction(__func__, F_START);
+  bool isValid = false;
+  string inStr = "";
+  float floatIn;
+
+  while (!isValid){
+    inStr = getPrompt(prompt);
+    if (isInt(inStr)){
+      floatIn = stof(inStr);
+      if (floatIn >= min && floatIn <= max){
+        isValid = true;
+      }else{
+        cout << FG_RED << to_string(floatIn) + " is not a valid option. Must be between " + to_string(min) + " and " + to_string(max) << RST;
+      }
+    }else{
+      cout << FG_RED << "You have entered an invalid option [" + inStr + "]. Please try again...\n\n" << RST;
+    }
+  }
+  myScribe.announceFunction(__func__, F_END);
+  return floatIn;
+}
+
+bool pigGame::promptYN(string prompt)
+{
+    bool isValid = false;
+    string strIn;
+	do{
+        cout << prompt;
+		getline(cin, strIn);
+	}while((strIn.length() != 1) || (tolower(strIn[0]) != 'n' && tolower(strIn[0]) != 'y' ));
+	if (tolower(strIn[0]) == 'y'){
+		return true;
+	}else{
+		return false;
+	}
+
+}
+
+bool pigGame::isInt(string inStr)
+{
+  myScribe.announceFunction(__func__, F_START);
+  bool isValid;
+
+  for (int ch = 0; ch <= inStr.size() - 1; ch++){
+    isValid = isdigit(inStr[ch]);
+  }
+  myScribe.announceFunction(__func__, F_END);
+  return isValid;
+}
+string pigGame::getPrompt(string prompt)
+{
+  myScribe.announceFunction(__func__, F_START);
+  string strIn;
+
+  cout << prompt;
+  cin >> strIn;
+  myScribe.announceFunction(__func__, F_END);
+  return strIn;
+}
+bool pigGame::exitGame()
+{
+	myScribe.announceFunction(__func__,"Game Exiting...");
+	return false;
+}
+// Specification A1 - D6() function
 int pigGame::D6()
 {
 	myScribe.announceFunction(__func__, F_START);
 	cout << "Rolling dice!\n";
 	int result = RandomNumber(1,6);
-	cout << "Player has rolled a " << result << endl;
+	cout << hPlayer << " has rolled a " << result << endl;
 	myScribe.announceFunction(__func__, F_END);
     return result;
 }
 
+// Specification A2 - RandomNumber() function
 int pigGame::RandomNumber(int hi, int low)
 {
 	myScribe.announceFunction(__func__, F_START);
@@ -156,3 +281,154 @@ int pigGame::RandomNumber(int hi, int low)
 	myScribe.announceFunction(__func__, F_END);
 	return result;
 }
+
+// Specification C2 - Student Name
+void pigGame::getPlayerName()
+{
+	bool name = false;
+	do{
+	cout << "What is your name (First Last)? ";
+	getline(cin, hPlayer);
+	cout << "Name entered: " << hPlayer << "\n";
+	name = promptYN("Is this correct? [Y/n]");
+	} while (!name);
+}
+
+void pigGame::startGame()
+{
+	getFirstPlayer();
+	do{
+		do{
+			getCurrentScores();
+			if (currentPlayer == PLAYER){
+				playerTurn();
+			}else{
+				AITurn();
+			}
+		}while(!turnEnd);
+		if (currentPlayer == PLAYER) currentPlayer = AI;
+		if (currentPlayer == AI) currentPlayer = PLAYER;
+	}while(player_grand_total < WIN && ai_grand_total < WIN);
+};
+
+void pigGame::rollDice()
+{
+	roll = D6();
+	if(currentPlayer == PLAYER){
+		cout << hPlayer << " rolled a " << roll << ".\n";
+		if (roll > 1){
+			cout << roll << " points added to score!";
+			player_total += roll;
+		}else{
+			cout << "Turn Lost!\n";
+			turnEnd = true;
+		}
+
+	}else{
+		cout << "AI Rolled a " << roll;
+		if (roll > 1){
+			cout << roll << " points added to score!";
+			ai_total += roll;
+		}else{
+			turnEnd = true;
+		}
+
+    }
+}
+
+void pigGame::getCurrentScores()
+{
+	cout <<  "-" << setw(20) << "Player" << "|" << setw(11) << "Total" << "|" << setw(11) << "Grand Total" << "-\n"
+	     << "|" << setw(20) << hPlayer << "|" << setw(11) << player_total << "|" << setw(11) << player_grand_total << "|\n"
+		 <<  "-" << string('-',20) << "|" << string('-',11) << "|" << string('-',21) << endl
+		 << "|" << setw(20) << "AI" << "|" << setw(11) << ai_total << "|" << setw(11) << ai_grand_total << "|\n"
+		 <<  "-" << string('-',20) << "|" << string('-',11) << "|" << string('-',21) << endl;
+} 
+
+void pigGame::getAction()
+{
+    getCurrentScores();
+	int choice = promptIntInRange("What would you like to do?\n\n\t1. Roll\n\t2. Hold\n\t3. Quit\n\nEnter selection and press ENTER: ",1,3);
+	actionChoice = static_cast<action>(choice);
+}
+
+void pigGame::playerTurn()
+{
+	getAction();
+	turnEnd = false;
+	if (actionChoice == HOLD){
+		player_grand_total += player_total;
+		turnEnd = true;
+	}else if(actionChoice == ROLL){
+		rollDice();
+	}else if(actionChoice == QUIT){
+		exit(0);
+	}
+}
+
+void pigGame::AITurn()
+{
+	int choice = D6();
+	turnEnd = false;
+	do{
+	if (choice >= 1 && choice <= 3){
+		turnEnd = true;
+		ai_grand_total += ai_total;
+	}else{
+		actionChoice = ROLL;
+		rollDice();
+	}}while(!turnEnd);
+}
+
+void pigGame::welcome()
+{
+	cout << "\n\tWelcom to Pig!\n\n" << PIG_ART << "\n\n";
+}
+
+void pigGame::getFirstPlayer()
+{
+	cout << "Let's get started by deciding who goes first.\n";
+	int pick = promptIntInRange("Choose heads or tails! [0 = heads, 1 = tails]",0,1);
+	if (pick){
+		cout << "The coin landed on tails!";
+	}else{
+		cout << "The coin landed on heads!";
+	}
+
+	bool flip = dice.coinFlip();
+
+	if (pick == flip){
+		cout << "Player goes first!";
+		currentPlayer = PLAYER;
+	}else{
+		cout << "The AI goes first!";
+		currentPlayer = AI;
+	}
+}
+
+pigGame::pigGame()
+{
+			myScribe.announceFunction(__func__, "Initializing Pig");
+			welcome();
+			getPlayerName();
+			game_count = 0;
+			do{
+				game_count++;
+				startGame();
+				// Specification A4 - Play Again Option
+				playAgain = promptYN((retry_menu + "\nWould you like to play again?"));
+			}while(playAgain);
+			myScribe.announceFunction(__func__, "Exiting Pig");
+}
+
+
+void jlogger::initLogFile()
+{
+	time_t t = std::time(nullptr);
+	tm tm = *std::localtime(&t);
+	cout << "New log started at " << put_time(&tm, "%F %T") << endl;
+	obLogFile << "New log started at " << put_time(&tm, "%F %T") << endl;
+}
+
+
+
